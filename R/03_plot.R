14f176cc3e1a1af06f76de3a76a2f649fd7094de
NatAnastas <anastasiadounat@gmail.com>
2025-04-26 11:39:56 +0300

    data

--- R/03_plot.R
+++ R/03_plot.R
@@ -26,11 +26,22 @@ colnames(df)
 
 #plot------
 
+# col = c("#00429d", "#73a2c6", '#396375', '#5a8192', '#6f6e9a', '#a2a0cf', '#e37b78',"#A65628", '#b24745')
+
+
+
+col = c(
+    "A" = "#B28B2D",
+    "B" = "#979d51",
+    "C" = "#79AF97",
+    "D" = "#748AA6"
+)
+
 df |>
      
-    ggplot(aes(x = Time, y = Number, fill = Type, color = Type, linetype = Type)) +
+    ggplot(aes(x = Time, y = Number, fill = Type)) +
   
-    geom_stream(type = "ridge", color = "white", lwd = 0.1, bw = .9) +
+    geom_stream(aes(color = Type), type = "ridge", lwd = 0.1, bw = .9) +
     
     # scale_y_continuous(labels = scales::comma) +
     
@@ -38,8 +49,8 @@ df |>
     
     # scale_x_continuous(expand = c(0, 0), breaks = seq(2003, 2023, by = 4), limits = c(2003, 2023)) +
   
-    scale_color_manual(values = c("#80539a", "#2293ca", "#656566")) +
-    scale_fill_manual(values = c("#80539a", "#2293ca", "#656566")) +
+    scale_color_manual(values = col) +
+    scale_fill_manual(values = col) +
 
     
     theme_minimal() +
@@ -48,7 +59,7 @@ df |>
         plot.background = element_rect(fill = "#e4e4e3"),
         legend.position = "none",
         panel.grid.major = element_line(linewidth = .35, color = "grey85"),
-        panel.grid.minor = element_line(linewidth = .25, color = "grey85", linetype = "dashed"),
+        panel.grid.minor = element_blank(),
         
         axis.title.y = element_text(margin = margin(r = 10)),
         axis.title.x = element_text(margin = margin(t = 10)),
