c72881e027281e379f94df0e506494fde703aed7
NatAnastas <anastasiadounat@gmail.com>
2025-07-09 13:26:00 +0300

    remove backround

--- R/02_logo.R
+++ R/02_logo.R
@@ -8,78 +8,29 @@ gc()
 # library -------------- 
 
 library(ggplot2)
-# library(dplyr)
 library(data.table)
-# library(readxl)
-# library(ggstream)
-# library(colorspace)
+
+install.packages("magick")
 
 
 # load data ----------
 
-df <- readxl::read_xlsx("Data.xlsx") |> setDT()
-# 
-# df1 <- data.table(
-#     x = c(2002, 2006, 2008),
-#     y = c(2150, 1800, 2300),
-#     size = c(23, 22, 25),  
-#     Type = c("D", "D", "D")
-# )
+library(magick)
 
+# Load your PNG image
+img <- image_read("odyssey_logo.png")
+
+# Make white background transparent
+# fuzz: tolerance level for what counts as "white" (increase if needed)
+img_transparent <- image_transparent(img, color = "white", fuzz = 20)
+
+# Save the output
+image_write(img_transparent, path = "logo_no_background.png", format = "png")
 
 # plot------
+library(rsvg)
+library(magick)
 
-# col = c("#00429d", "#73a2c6", '#396375', '#5a8192', '#6f6e9a', '#a2a0cf', '#e37b78',"#A65628", '#b24745')
+# Render SVG at high resolution
+img_svg <- image_read_svg("Odyssey_logo.svg", width = 1000, height = 1000)
 
-col = c(
-    "A" = "#926f0a",
-    "B" = "#979d51",
-    "C" = "#526E2D",
-    "D" = "#677d99"
-)
-
-g = df |>
-     
-    ggplot(aes(x = Time, y = Number, fill = Type)) +
-  
-    geom_stream(color = "white", type = "ridge", lwd = 0.5, bw = .9) +
-    
-    # geom_point(
-    #     data = df1,
-    #     aes(x = x, y = y, size = size),
-    #     shape = 21,
-    #     stroke = .2,
-    #     color = "white"
-    # ) +
-
-    scale_size_continuous(range = c(15,30)) +
-
-    # scale_y_continuous(limits = c(0, 3000), breaks = seq(0, 3000, by = 500)) +
-
-    # scale_color_manual(values = col) +
-    scale_fill_manual(values = col) +
-
-    
-    theme_minimal() +
-  
-    theme(
-        plot.background = element_rect(fill = "#e4e4e3"),
-        legend.position = "none",
-        
-        
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.title = element_blank(),
-        axis.text = element_blank(),
-        
-        plot.margin = margin(20, 20, 20, 20)
-    ) 
-      
-g
-
-
-# install.packages('svglite')
-# library(svglite)
-
-# # Save the plot with custom size and resolution
-ggsave("logo.png", plot = g, width = 15, height = 10, dpi = 600)
